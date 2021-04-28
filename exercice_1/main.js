0759a54f3dc56b71820e599f701a953565fd768c
katherineburgain <68737754+burgerkati@users.noreply.github.com>
2021-04-28 11:55:49 +0200

    change id

--- exercice_1/main.js
+++ exercice_1/main.js
@@ -1,4 +1,4 @@
 // Au clic sur le bouton, afficher une boite de dialogue avec le message de votre choix.
-$("#clique-moi").click(function(){
+$("#clickButton").click(function(){
   alert('"A penny saved is a penny earned."');
 });
